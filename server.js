e94c755cba629fd530cc40ad3494da251ff25e93
Huzaifa9559 <huzaifanaseer03@gmail.com>
2024-03-14 23:36:49 +0500

    first Comment

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const connectDB = require('./db/db');
-require('dotenv').config();
+// const connectDB = require('./db/db');
+// require('dotenv').config();
 
 var app = express();
-connectDB();
-app.use('/uploads', express.static('uploads'))
+// connectDB();
+// app.use('/uploads', express.static('uploads'))
 
-var port = process.env.PORT || 5000;
+// var port = process.env.PORT || 5000;
 
 const cors = require('cors')
 
@@ -22,13 +22,13 @@ app.get('/', (req, res) => {
     res.json({ msg: 'Server Running....' })
 })
 
-app.use('/register', require('./routes/user'));
-app.use('/familyRegister', require('./routes/family'))
-app.use('/login', require('./routes/auth'));
-app.use('/mailVerify', require('./routes/userEmailVerification'))
-app.use('/doorUnlock', require('./routes/door_lock'));
+// app.use('/register', require('./routes/user'));
+// app.use('/familyRegister', require('./routes/family'))
+// app.use('/login', require('./routes/auth'));
+// app.use('/mailVerify', require('./routes/userEmailVerification'))
+// app.use('/doorUnlock', require('./routes/door_lock'));
 
-app.use('/forget', require('./routes/forgetPassword'))
+// app.use('/forget', require('./routes/forgetPassword'))
 
 app.listen(3000, () => {
     console.log("Server Started on Port: ", 3000);
